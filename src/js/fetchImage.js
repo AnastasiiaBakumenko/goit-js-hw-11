97ec38f943b0fabf52e13cb230a061d1ffe0015f
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-02-07 19:22:48 +0100

    add homework 11

---
+++ src/js/fetchImage.js
@@ -0,0 +1,36 @@
+const axios = require('axios').default;
+
+export default class FetchImage {
+  #BASE_URL = 'https://pixabay.com';
+  #URL = 'api/';
+  #KEY = '33369895-08f083aac748bf074ebfa9156';
+  #PER_PAGE = 40;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: this.#BASE_URL,
+      timeout: 1000,
+    });
+    this.curentPage = 1;
+    this.curentTextQuery = '';
+  }
+
+  getParamReguest(textQuery) {
+    return {
+      key: this.#KEY,
+      image_type: 'photo',
+      pretty: true,
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: this.#PER_PAGE,
+      page: this.curentPage,
+      q: this.curentTextQuery,
+    };
+  }
+
+  getImage() {
+    return this.instance.get(this.#URL, {
+      params: this.getParamReguest(),
+    });
+  }
+}
